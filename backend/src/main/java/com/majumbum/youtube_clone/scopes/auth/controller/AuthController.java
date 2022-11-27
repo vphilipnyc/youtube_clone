11e562e485d54a6b025330b8ed78d77198855fe6
Victor Philip <5893811+vphilipnyc@users.noreply.github.com>
2022-11-26 19:04:22 -0500

    no var needed

--- backend/src/main/java/com/majumbum/youtube_clone/scopes/auth/controller/AuthController.java
+++ backend/src/main/java/com/majumbum/youtube_clone/scopes/auth/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -59,7 +60,7 @@ public class AuthController {
         String jwt = tokenService.createToken(user);
 
         List<String> roles = user.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+                .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
 
